dbb7eb1bbc9b2424635b83bdcb4f8fef09975247
Dharmin Shah <shahdk@rose-hulman.edu>
2013-09-25 16:44:35 -0400

    Created project

---
+++ MeetingBuilder/MeetingBuilder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeetingBuilder
+//
+//  Created by CSSE Department on 9/25/13.
+//  Copyright (c) 2013 CSSE371. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iWinAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iWinAppDelegate class]));
+    }
+}
