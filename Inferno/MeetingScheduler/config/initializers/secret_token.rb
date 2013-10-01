fbf66046867cd973fa4c26239c9e0e97d7c6eeea
Lindsey Watterson <watterlm@rose-hulman.edu>
2013-10-01 12:03:50 -0400

    created file for team Inferno and beginning of Ruby setup

---
+++ Inferno/MeetingScheduler/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetingScheduler::Application.config.secret_key_base = '5e9767ed44fd7f6ffac90890b698db308ca5256afef4545c1e6e20c25d055abebfd189f8581e70f05d735cf5ff92087aba5aa5ed3691081d50652778850c46a9'
