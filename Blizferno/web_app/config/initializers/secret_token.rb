0d293a205b066cd25d32b7916dc1e9685207703a
harbisjs <harbisjs@rose-hulman.edu>
2013-10-22 02:12:03 -0400

    Added intial web prototype

---
+++ Blizferno/web_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebApp::Application.config.secret_key_base = '9de67b25c6e628f43234eece9903f45faf2c3b302a40c15477f23013768110c09df498f272d92799c1149801fc8cbceb0e797bca2a0a965f6718f7986671a869'
