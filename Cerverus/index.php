fc9320aedf281118c5077ab115b57476f781e48a
Paulbliudzius <bliudzpp@rose-hulman.edu>
2013-10-30 21:27:56 -0400

    updateUser - in trial

--- Cerverus/index.php
+++ Cerverus/index.php
@@ -41,5 +41,47 @@ if( strcasecmp($_GET['method'],'login') == 0){
 	
 	$response= $userIndex->add($userNode, 'user', $userProps['name']);
 	echo $response;
+}else if(strcasecmp($_GET['method'], 'updateUser') ==0){
+	//get the json string post content
+	$postContent = json_decode(@file_get_contents('php://input'));
+	
+//	$index= new IndexService($graphDb);
+
+	$nodes= $userIndex->getNodes("Users", "email", $_GET['email'] );
+	if(sizeof($nodes) >0){
+		$node = $nodes[0];
+		if(strcasecmp($postContent->field, 'password') ==0){
+			$node->password = "". $postContent->value;
+			$node->save();
+			echo $node->getProperties();
+			//continue this if/else statement for all other fields in the statement
+			/*
+			localhost?method=updateUser&user=paul
+			{"field":"password", "value":"######"}
+			*/
+		}else if(strcasecmp($postContent->field, 'name') ==0){
+			$node->name = "". $postContent->value;
+			$node->save();
+			echo $node->getProperties();
+		}else if(strcasecmp($postContent->field, 'company') ==0){
+			$node->company = "". $postContent->value;
+			$node->save();
+			echo $node->getProperties();
+		}else if(strcasecmp($postContent->field, 'phone') ==0){
+			$node->phone = "". $postContent->value;
+			$node->save();
+			echo $node->getProperties();
+		}else if(strcasecmp($postContent->field, 'username') ==0){
+			$node->username = "". $postContent->value;
+			$node->save();
+			echo $node->getProperties();
+		}else{
+			echo "no node updated";
+		}
+		
+
+	}else{
+		echo "FALSE node not found";
+	}
 }
 ?>
