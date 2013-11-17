5b574042b2a2f11f80d94cee0e2de860b32460e7
smithgb <g_rant@dhcp-137-112-236-236.wlan.rose-hulman.edu>
2013-11-17 13:46:22 -0500

    Bootstrap UI Development
    +added ip whitelist feature
      -restrict site access by ip
    +added collapsing pane
      -used bootstrap collapse to make homepage
      -4 colored buttons
    +added test pane
      -render pane to test bootstrap components in views
    +modularized mypage.html
      -moved previous content to its own view pane for rails rendering

--- Blizferno/web_app/app/controllers/application_controller.rb
+++ Blizferno/web_app/app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout 'slate'
+
+  before_filter :protect
+
+  def protect
+  	@ips = ['127.0.0.1'] #and so on...]
+  	if not @ips.include? request.remote_ip
+  		# check for your subnet stuff here, for example
+  		# if not request.remote_ip.include?('127.0,0')
+  		render text: "You Are Unauthorized"
+  		return
+  	end
+  end
 end
