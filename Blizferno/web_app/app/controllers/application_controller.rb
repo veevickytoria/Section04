a561580ff488bc625341790ad8a39afa5f9cce35
smithgb <g_rant@dhcp-137-112-147-103.reshall.rose-hulman.edu>
2013-11-18 16:48:19 -0500

    Polishing & Organizing
    Polished routing.
    Organized navbars.

--- Blizferno/web_app/app/controllers/application_controller.rb
+++ Blizferno/web_app/app/controllers/application_controller.rb
@@ -7,11 +7,14 @@ class ApplicationController < ActionController::Base
   before_filter :protect
 
   def protect
-  	@ips = [] #and so on...]
-  	if not @ips.include? request.remote_ip
+    roseNetworkIP = IPAddr.new '137.0.0.0/8'
+    #clientIP = IPAddr.new 'xxx.xxx.xxx/X'
+  	@ips = ['127.0.0.1'] #and so on...]
+    userIP = request.remote_ip
+  	if not (@ips.include? userIP or roseNetworkIP.include? userIP)
   		# check for your subnet stuff here, for example
   		# if not request.remote_ip.include?('127.0,0')
-  		render text: '<h1>The IP of <i>' + request.remote_ip + '</i> is unauthorized to access this section.</h1><br />Please contact system administrator if you believe you have reached this page in error.'
+  		render text: '<h1>The IP of <i>' + userIP + '</i> is unauthorized to access this section.</h1><br />Please contact system administrator if you believe you have reached this page in error.'
   		return
   	end
   end
