90be33ef3b93774324a3600d66846980d66b46d2
vattercw <user@users-Mac-mini.local>
2013-12-04 13:15:42 -0500

    Added task list page
    I think I've committed  this before but my GitHub says I haven't I hope
    this doesn't screw anything up. This is the task list addition from a
    few weeks ago.

---
+++ Blizferno/web_app/test/controllers/task_list_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TaskListControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ Blizferno/web_app/test/helpers/task_list_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TaskListHelperTest < ActionView::TestCase
+end
