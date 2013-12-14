b4915f5fa5b3ec4a41dfc3e4ca0ea9fb9c06a726
hazzargm <csse@cssemm-04.belkin>
2013-12-13 19:29:18 -0500

    Added EditProfile Group

---
+++ MeetingBuilder/MeetingBuilder/iWinEditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  iWinEditProfileViewController.h
+//  MeetingBuilder
+//
+//  Created by CSSE Department on 12/13/13.
+//  Copyright (c) 2013 CSSE371. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iWinEditProfileViewController : UIViewController
+
+@end
---
+++ MeetingBuilder/MeetingBuilder/iWinEditProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  iWinEditProfileViewController.m
+//  MeetingBuilder
+//
+//  Created by CSSE Department on 12/13/13.
+//  Copyright (c) 2013 CSSE371. All rights reserved.
+//
+
+#import "iWinEditProfileViewController.h"
+
+@interface iWinEditProfileViewController ()
+
+@end
+
+@implementation iWinEditProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
