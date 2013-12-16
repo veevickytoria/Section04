1cb3e290134285e1caeae5dd15a9c85d0fe51fa6
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2013-12-15 21:11:01 -0500

    GET REQUESTS!!!
    GRANT SMITH
    COREY VATTER
    FTW

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -1,10 +1,15 @@
 class ProfileController < ApplicationController
 	def index
 		require 'net/http'
-		url = URI('http://csse371-04.csse.rose-hulman.edu/User/350')
-		params = { :email => 10}
-		Net::HTTP.get(url)
-
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/350/')
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+		@stuff = res.body
+		# http = Net::HTTP.new(uri.host, uri.port)
+		# request = Net::HTTP::Get.new(uri.request_uri)
+		# response = http.request(request)
 	end
 	def editmyprofile 
 	end
