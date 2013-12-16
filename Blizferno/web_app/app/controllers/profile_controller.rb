a9c10bfa47635c4206d7f96d1ce5c1ad984b7bff
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2013-12-15 20:33:10 -0500

    more profile stuff
    grant smith
    corey vatter

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -2,11 +2,12 @@ class ProfileController < ApplicationController
 	def index
 		require 'net/http'
 		url = URI('http://csse371-04.csse.rose-hulman.edu/User/350')
+		params = { :email => 10}
 		Net::HTTP.get(url)
-		@mymothafuckinemail = params[:email]
+
 	end
 	def editmyprofile 
 	end
 	layout 'slate'
-	
+
 end
