3b3da6f1c64fd326ed7b44ac6815f12085c79ccf
watterlm <watterlm@rose-hulman.edu>
2013-12-16 23:17:57 -0500

    Task viewing

--- Blizferno/web_app/app/controllers/tasks_controller.rb
+++ Blizferno/web_app/app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < ApplicationController
 	def index
+		require 'net/http'
+		@UserID = '350'
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Tasks/' + @UserID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+		#@tasks = JSON.parse(res.body)
+		@tasks = JSON.parse('{"tasks":[{"taskID":"1","title":"Delete Task","project":"Meeting Ninja","deadline":"19-Dec-13"},
+			{"taskID":"2","title":"Create a Meeting","project":"Meeting Ninja","deadline":"19-Dec-13"},
+			{"taskID":"3","title":"Create a Task","project":"Meeting Ninja","deadline":"19-Dec-13"},
+			{"taskID":"4","title":"View a Task","project":"Meeting Ninja","deadline":"19-Dec-13"}]}')
+		@TaskArray = ""
 	end
 
 	def list
