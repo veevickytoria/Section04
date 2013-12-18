51ba3df777dfa1e76033de88a15993f31e51fc35
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2013-12-17 21:10:25 -0500

    Cookies Work
    successfully stored userID from backend for access across site
    COREY VATTER & GRANT SMITH

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -1,17 +1,6 @@
 class ProfileController < ApplicationController
 	def index
-		require 'net/http'
-		#url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/@userID/')
-		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/717')
-		req = Net::HTTP::Get.new(url.path)
-		res = Net::HTTP.start(url.host, url.port) {|http|
-			http.request(req)
-		}
-		#@me = {'name'=>'jerry','title'=>'boss'}
-		@stuff = JSON.parse(res.body)
-		# http = Net::HTTP.new(uri.host, uri.port)
-		# request = Net::HTTP::Get.new(uri.request_uri)
-		# response = http.request(request)
+		@userTasks = {'tasks'=>[{'taskName'=>'Memo'},{'taskName'=>'Flyer'},{'taskName'=>'Drink Beer'},{'taskName'=>'Learn Japanese'},{'taskName'=>'Code Website'}]}
 	end
 	def editmyprofile 
 	end
