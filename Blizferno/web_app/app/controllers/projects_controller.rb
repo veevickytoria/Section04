dd2692bf1eb91a8c952c9ec21fd09d557d5792b2
Christopher <oharace@rose-hulman.edu>
2014-01-13 15:46:06 -0500

    Created main Projects page, updated homepage

--- Blizferno/web_app/app/controllers/projects_controller.rb
+++ Blizferno/web_app/app/controllers/projects_controller.rb
@@ -1,4 +1,28 @@
 class ProjectsController < ApplicationController
   def index
+  	if (cookies[:userID].blank?)
+			redirect_to '/login/index'
+			return
+		end
+		require 'net/http'
+		@UserID = '717'
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Tasks/' + @UserID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+
+		@projects = JSON.parse('{"projects":[{"projectID":"1","name":"Project Uno","group":"Web"},
+											 {"projectID":"2","name":"Project Dos","group":"Backend"},
+											 {"projectID":"3","name":"Project Tres","group":"Android"},
+											 {"projectID":"4","name":"Project Cuatro","group":"iOS"}]}')
+
   end
+
+  def list
+  end
+
+  def new
+  end
+  layout 'slate'
 end
