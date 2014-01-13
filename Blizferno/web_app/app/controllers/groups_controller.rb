2f82c48f4971bfbc24564205d71c4f87919d81f5
vattercw <vattercw@rose-hulman.edu>
2014-01-12 22:37:46 -0500

    Got the main groups and most of view to work
    still a little bit of a bug on viewing groups but the progress is there.

--- Blizferno/web_app/app/controllers/groups_controller.rb
+++ Blizferno/web_app/app/controllers/groups_controller.rb
@@ -2,7 +2,23 @@ class GroupsController < ApplicationController
 	def index
 		if (cookies[:userID].blank?)
 			redirect_to '/login/index'
+			return
 		end
+		require 'net/http'
+		@UserID = '717'
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Groups/' + @UserID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+
+
+		#@tasks = JSON.parse(res.body)
+		@groups = JSON.parse('{"groups":[{"groupID":"1","groupTitle":"NWA","groupType":"hardcore gangsta"},
+			{"groupID":"2","groupTitle":"Group? More like poop!","groupType":"lolz"},
+			{"groupID":"3","groupTitle":"KKK","groupType":"racist"},
+			{"groupID":"4","groupTitle":"Murdah Squad","groupType":"serial killer"}]}')
+
 	end
 
 	def new
