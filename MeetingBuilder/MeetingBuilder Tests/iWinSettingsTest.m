862b1363d82d11717ac4c33797491ef5fd49a8a3
hazzargm <hazzargm@rose-hulman.edu>
2014-01-15 21:44:38 -0500

    Added save buttons. -Gordon

--- MeetingBuilder/MeetingBuilder Tests/iWinSettingsTest.m
+++ MeetingBuilder/MeetingBuilder Tests/iWinSettingsTest.m
@@ -8,9 +8,14 @@
 
 #import <XCTest/XCTest.h>
 #import "iWinViewAndChangeSettingsViewController.h"
+#import "iWinAppDelegate.h"
+#import "Contact.h"
+#import "Settings.h"
 
 @interface iWinSettingsTest : XCTestCase
 @property (strong, nonatomic) iWinViewAndChangeSettingsViewController *settingsVC;
+@property (strong, nonatomic) Contact *contact;
+@property (strong, nonatomic) Settings *settings;
 @end
 
 @implementation iWinSettingsTest
@@ -22,6 +27,38 @@
     self.settingsVC = [[iWinViewAndChangeSettingsViewController alloc] initWithNibName:@"iWinViewAndChangeSettingsViewController" bundle:nil withID:1];
     [self.settingsVC viewDidLoad];
     [self.settingsVC view];
+    
+    iWinAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entityDesc = [NSEntityDescription entityForName:@"Contact" inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDesc];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"userID = 1"];
+    [request setPredicate:predicate];
+    
+    NSError *error;
+    NSArray *result = [context executeFetchRequest:request
+                                             error:&error];
+    self.contact = (Contact*)[result objectAtIndex:0];
+    
+    
+    
+    NSEntityDescription *entityDesc1 = [NSEntityDescription entityForName:@"Settings" inManagedObjectContext:context];
+    
+    NSFetchRequest *request1 = [[NSFetchRequest alloc] init];
+    [request1 setEntity:entityDesc1];
+    
+    NSPredicate *predicate1 = [NSPredicate predicateWithFormat:@"userID = 1"];
+    [request1 setPredicate:predicate1];
+    
+    NSError *error1;
+    NSArray *result1 = [context executeFetchRequest:request1
+                                              error:&error1];
+    self.settings = (Settings*)[result1 objectAtIndex:0];
 
 }
 
@@ -33,32 +70,91 @@
 
 - (void)testEmailChangeCorrectOldPassword
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"EMAIL";
+    self.settingsVC.oldPasswordTextField.text = @"123456";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    XCTAssertTrue([self.settings.email isEqualToString:@"EMAIL"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"EMAIL"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"123456"], @"Check failed");
+
+
+    
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"shahdk@rose-hulman.edu";
+    self.settingsVC.oldPasswordTextField.text = @"123456";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    XCTAssertTrue([self.settings.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"123456"], @"Check failed");
+
+
 }
 
 - (void)testEmailChangeIncorrectOrNoOldPassword
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"EMAIL";
+    self.settingsVC.oldPasswordTextField.text = @"NotRightPassword";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    XCTAssertTrue([self.settings.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"123456"], @"Check failed");
 
-- (void)testPasswordChangeCorrectOldPassword
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-- (void)testPasswordChangeIncorrectOrNoOldPassword
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 - (void)testEmailAndPasswordChangeCorrectPassword
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"EMAIL";
+    self.settingsVC.oldPasswordTextField.text = @"123456";
+    self.settingsVC.passwordTextField.text = @"newPass12";
+    self.settingsVC.confirmPasswordTextField.text = @"newPass12";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    XCTAssertTrue([self.settings.email isEqualToString:@"EMAIL"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"EMAIL"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"newPass12"], @"Check failed");
+
+
+    
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"shahdk@rose-hulman.edu";
+    self.settingsVC.oldPasswordTextField.text = @"newPass12";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    XCTAssertTrue([self.settings.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"newPass12"], @"Check failed");
+
+
+    
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.oldPasswordTextField.text = @"newPass12";
+    self.settingsVC.passwordTextField.text = @"123456";
+    self.settingsVC.confirmPasswordTextField.text = @"123456";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+
+    XCTAssertTrue([self.settings.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"123456"], @"Check failed");
 }
 
 - (void)testEmailAndPasswordChangeIncorrectOrNoOldPassword
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    self.settingsVC.emailTextField.text = @"EMAIL";
+    self.settingsVC.oldPasswordTextField.text = @"NotRightPassword";
+    self.settingsVC.passwordTextField.text = @"newPass12";
+    self.settingsVC.confirmPasswordTextField.text = @"newPass12";
+    [self.settingsVC.saveAndEditButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+
+    XCTAssertTrue([self.settings.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.contact.email isEqualToString:@"shahdk@rose-hulman.edu"], @"Check failed");
+    XCTAssertTrue([self.settings.password isEqualToString:@"123456"], @"Check failed");
 }
 
 @end
--- MeetingBuilder/MeetingBuilder/iWinViewAndChangeSettingsViewController.h
+++ MeetingBuilder/MeetingBuilder/iWinViewAndChangeSettingsViewController.h
@@ -24,16 +24,24 @@
 - (IBAction)onCancel:(id)sender;
 - (IBAction)onEdit:(id)sender;
 - (IBAction)onDelete:(id)sender;
+- (IBAction)onSaveSwitch:(id)sender;
+- (IBAction)onSaveTable:(id)sender;
 
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *oldPasswordTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+
 @property (weak, nonatomic) IBOutlet UITableView *whenToNotifyPicker;
+
 @property (weak, nonatomic) IBOutlet UISwitch *shouldNotifySwitch;
+
 @property (weak, nonatomic) IBOutlet UIButton *cancelButton;
 @property (weak, nonatomic) IBOutlet UIButton *saveAndEditButton;
 @property (weak, nonatomic) IBOutlet UIButton *deleteButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *saveTable;
+
 @property (weak, nonatomic) IBOutlet UILabel *confirmPasswordLabel;
 @property (weak, nonatomic) IBOutlet UILabel *passwordLabel;
 @property (weak, nonatomic) IBOutlet UILabel *oldPasswordLabel;
