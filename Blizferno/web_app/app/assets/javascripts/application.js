421e83df1f7f08eddf23bae1650aa51023c0bcfe
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2014-01-18 11:14:01 -0500

    Revert 147a713..e5018b2
    This rolls back to commit 147a7135e44f5e357c2d819b7fe9d8222842d62d.

--- Blizferno/web_app/app/assets/javascripts/application.js
+++ Blizferno/web_app/app/assets/javascripts/application.js
@@ -10,5 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
-//= require json2.js
+=require json2.js
+
