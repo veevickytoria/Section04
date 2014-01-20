b6c32336df74aa3003541dfce3aacf58afe84292
harbisjs <harbisjs@rose-hulman.edu>
2014-01-19 23:21:50 -0500

    first commit in a long time. working on scheduler and editProfile -Josh & JEarles

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -7,6 +7,14 @@ class ProfileController < ApplicationController
 		@userTasks = {'tasks'=>[{'taskTitle'=>'Memo'},{'taskTitle'=>'Flyer'},{'taskTitle'=>'Drink Beer'},{'taskTitle'=>'Learn Japanese'},{'taskTitle'=>'Code Website'}]}
 		@userMeetings = {'meetings'=>[{'meetingTitle'=>'Board Meeting'},{'meetingTitle'=>'Bluth Account Meeting'},{'meetingTitle'=>'Consult JCS'}]}
 		@userProjects = {'projects'=>[{'projectTitle'=>'Student Schedule'},{'projectTitle'=>'371 Project'}]}
+
+
+
+
+
+require 'net/http'
+result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Schedule/749'))
+@userSchedule = result
 	end
 	def editmyprofile 
 	end
