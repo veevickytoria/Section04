b56ea159e25582c2819814fcb2f6156a0eb22589
william brophy <brophy.will@yahoo.com>
2014-01-24 13:21:22 -0500

    changed simpleuser and group to implement parcelable

--- DroidRage/src/objects/SimpleUser.java
+++ DroidRage/src/objects/SimpleUser.java
@@ -1,16 +1,26 @@
 package objects;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "userID", "displayName" })
-public class SimpleUser {
+public class SimpleUser implements Parcelable{
 	@JsonProperty("userID")
 	protected String userID;
 	@JsonProperty("displayName")
 	protected String displayName;
+	
+	public SimpleUser(){
+	}
+	
+	public SimpleUser(Parcel in){
+		readFromParcel(in);
+	}
 
 	@JsonProperty("userID")
 	public String getUserID() {
@@ -37,4 +47,35 @@ public class SimpleUser {
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(getUserID());
+		dest.writeString(getDisplayName());
+	}
+	
+	public void readFromParcel(Parcel in){
+		setUserID(in.readString());
+		setDisplayName(in.readString());
+	}
+	
+	public static final Parcelable.Creator<SimpleUser> CREATOR = new Parcelable.Creator<SimpleUser>() {
+
+		@Override
+		public SimpleUser createFromParcel(Parcel in) {
+			return new SimpleUser(in);
+		}
+
+		@Override
+		public SimpleUser[] newArray(int size) {
+			return new SimpleUser[size];
+		}
+		
+	};
 }
