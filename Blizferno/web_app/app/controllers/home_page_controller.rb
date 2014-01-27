a020e20651f40fed983b800bb1b7b43e61a6c3ad
vattercw <vattercw@rose-hulman.edu>
2014-01-26 19:33:49 -0500

    got the modals to show up on front page and used ruby code to limit number of each item that shows up.

--- Blizferno/web_app/app/controllers/home_page_controller.rb
+++ Blizferno/web_app/app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
 class HomePageController < ApplicationController
+
+	before_filter :getMeetings
+	
 	def index
 		if (cookies[:userID].blank?)
 			redirect_to '/login/index'
@@ -20,5 +23,36 @@ class HomePageController < ApplicationController
 	end
 	def mypage
 	end
+
+	def getMeetings
+		require 'net/http'
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Meetings/' + @userID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+		getUserMeetings = JSON.parse(res.body)
+		@meetings = Array.new
+		@meetingsParsed = Array.new
+		@meetingString = ''
+
+		getUserMeetings['meetings'].each do |meeting|
+			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Meeting/' + meeting['id'].to_s)
+			req = Net::HTTP::Get.new(url.path)
+			res = Net::HTTP.start(url.host, url.port) {|http|
+				http.request(req)
+			}
+			@meetingString = res.body
+			meetingIdString = ',"meetingID":"'+meeting['id'].to_s+'"}';
+
+			@meetingString = @meetingString[0..-2] + meetingIdString;
+
+			@meetings.push(@meetingString)
+			@meetingsParsed.push(JSON.parse(@meetingString))
+		end
+
+		
+	end
+
 	layout 'slate'
 end
