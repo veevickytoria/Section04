cb883edc5309b54946537e234c02939f1f1969d6
someoneseth <rujirasl@rose-hulman.edu>
2014-01-29 16:24:49 -0500

    added PUT header for web team

--- Cerverus/User.php
+++ Cerverus/User.php
@@ -27,7 +27,7 @@ define("HASH_PBKDF2_INDEX", 3);
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
