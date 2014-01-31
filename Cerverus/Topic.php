3e9ac8b0c4c1bd41c6bfe9869af4af72d8788dde
yuhasem <yuhasem@rose-hulman.edu>
2014-01-31 00:20:26 -0500

    topic

--- Cerverus/Topic.php
+++ Cerverus/Topic.php
@@ -21,9 +21,6 @@ function createTopic($title, $time, $subtopic){
 	//createTopic method
 	//paramas: title, time, subtopics
 	
-	/*get the json string post content
-	$postContent = json_decode(@file_get_contents('php://input'));*/
-	
 	//create the node
 	$topicNode= $client->makeNode();
 	
@@ -55,40 +52,40 @@ function deleteTopic($id){
 	//deleteTopic
 	//parmas: id
         
-        //get the node
-        $node = $client->getNode($id);
-        //make sure the node exists
-        if($node != NULL){
-                //check if node has topic index
-                $topic = $topicIndex->findOne('ID', ''.$id);
-                                
-                //only delete the node if it's a topic
-                if($topic != NULL){
-                        //get the relationships
-                        $relations2 = $topic->getRelationships(array('HAS_TOPIC'));
-                        foreach($relations2 as $rel){
-                        	$toDelID = $rel->getEndNode()->getID();
-                        	deleteTopic($toDelID);
-                        	$rel->delete();
-                        }
-                        $relations = $topic->getRelationships();
-                        foreach($relations as $rel){
-                                //remove all relationships
-                                $rel->delete();
-                        }                
+    //get the node
+    $node = $client->getNode($id);
+    //make sure the node exists
+    if($node != NULL){
+             //check if node has topic index
+             $topic = $topicIndex->findOne('ID', ''.$id);
+                              
+             //only delete the node if it's a topic
+    		if($topic != NULL){
+                //get the relationships
+                $relations2 = $topic->getRelationships(array('HAS_TOPIC'));
+            	foreach($relations2 as $rel){
+                   	$toDelID = $rel->getEndNode()->getID();
+                   	deleteTopic($toDelID);
+                   	$rel->delete();
+                }
+                $relations = $topic->getRelationships();
+                foreach($relations as $rel){
+                    //remove all relationships
+                    $rel->delete();
+                }                
                         
-                        //delete node and return true
-                        $topic->delete();
-                  	    $array = array('valid'=>'true');
- 						echo json_encode($array);
- 						return 'true';
-                } else {
-                        //return an error otherwise
-                        $errorarray = array('errorID' => '4', 'errorMessage'=>'Given node ID is not a topic node');
-                        return 4;
- 				}
-		echo json_encode($errorarray);
-		} else {
+                //delete node and return true
+                $topic->delete();
+                $array = array('valid'=>'true');
+ 				echo json_encode($array);
+ 				return 'true';
+            } else {
+                //return an error otherwise
+                $errorarray = array('errorID' => '4', 'errorMessage'=>'Given node ID is not a topic node');
+                return 4;
+ 			}
+	echo json_encode($errorarray);
+	} else {
      	//return an error if ID doesn't point to a node
 		echo '{"errorID":"5", "errorMessage":"Given node ID is not recognized in database"}';
 		$errorarray = array('errorID' => '5', 'errorMessage'=>'Given node ID is not recognized in database');
