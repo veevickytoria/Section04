49711fa0c18420371f6283aa056a22fbf3d964e3
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2014-02-02 22:29:18 -0500

    Document Management Stuff

--- Blizferno/web_app/app/controllers/documents_controller.rb
+++ Blizferno/web_app/app/controllers/documents_controller.rb
@@ -1,11 +1,12 @@
 class DocumentsController < ApplicationController
-	layout "slate"
-
-	@notes = JSON.parse('{"notes": [{"noteID":"1","title":"@titleText","description":"@description","content":"@contentText","dateCreated":"@dateCreated"},
+	def index
+		@notes = JSON.parse('{"notes": [{"noteID":"1","title":"@titleText","description":"@description","content":"@contentText","dateCreated":"@dateCreated"},
 			{"noteID":"2","title":"@titleText","description":"@description","content":"@contentText","dateCreated":"@dateCreated"},
 			{"noteID":"3","title":"@titleText","description":"@description","content":"@contentText","dateCreated":"@dateCreated"}]}')
 
-	@agendas = JSON.parse('{"agendas":[{"agendaID":"1","title": "agenda1","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]},
+		@agendas = JSON.parse('{"agendas":[{"agendaID":"1","title": "agenda1","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]},
 			{"agendaID":"2","title": "agenda2","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]}]}')
 
+	end
+	layout "slate"
 end
