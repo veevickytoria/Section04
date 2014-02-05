a3eebb3d9794d93ab52051fa872723d0c23740ca
vattercw <vattercw@rose-hulman.edu>
2014-02-04 20:46:11 -0500

    Working on some profile melarchy
    Removed the profile page, pulling from backend for profile info

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -4,16 +4,21 @@ class ProfileController < ApplicationController
 			redirect_to '/login/index'
 			return
 		end
-		@userProjects = {'projects'=>[{'projectTitle'=>'Student Schedule'},{'projectTitle'=>'371 Project'}]}
-		@userGroups = {'groups'=>[{'groupTitle'=>'IAIT'},{'groupTitle'=>'ChandanSrPrj'},{'groupTitle'=>'FIJISucks'}]}
-
-
+		# @userProjects = {'projects'=>[{'projectTitle'=>'Student Schedule'},{'projectTitle'=>'371 Project'}]}
+		# @userGroups = {'groups'=>[{'groupTitle'=>'IAIT'},{'groupTitle'=>'ChandanSrPrj'},{'groupTitle'=>'FIJISucks'}]}
 
 
 
 require 'net/http'
-result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Schedule/749'))
+result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Schedule/'+cookies[:userID]+''))
 @userSchedule = result
+
+result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Groups/'+cookies[:userID]+''))
+@userGroups = result
+
+esult = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Projects/'+cookies[:userID]+''))
+@userProjects = result
+
 	end
 	def editmyprofile 
 	end
