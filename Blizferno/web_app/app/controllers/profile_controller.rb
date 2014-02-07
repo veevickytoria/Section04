92b21e8006636428c44f82d9294cb35b17e3a4f3
vattercw <vattercw@rose-hulman.edu>
2014-02-07 18:39:58 -0500

    Profile can now display all users (That aren't fucking blank)
    with some assistance from Grant (for guidance)

--- Blizferno/web_app/app/controllers/profile_controller.rb
+++ Blizferno/web_app/app/controllers/profile_controller.rb
@@ -4,20 +4,58 @@ class ProfileController < ApplicationController
 			redirect_to '/login/index'
 			return
 		end
-		# @userProjects = {'projects'=>[{'projectTitle'=>'Student Schedule'},{'projectTitle'=>'371 Project'}]}
-		# @userGroups = {'groups'=>[{'groupTitle'=>'IAIT'},{'groupTitle'=>'ChandanSrPrj'},{'groupTitle'=>'FIJISucks'}]}
 
 
 
-require 'net/http'
-result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Schedule/'+cookies[:userID]+''))
-@userSchedule = result
+		require 'net/http'
+		 urlSchedule = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Schedule/'+cookies[:userID]))
+		 @userSchedule = urlSchedule
 
-result = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Groups/'+cookies[:userID]+''))
-@userGroups = result
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Groups/' + cookies[:userID])
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+		@groupIDs = JSON.parse(res.body)
+		@groups = Array.new
 
-esult = Net::HTTP.get(URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Projects/'+cookies[:userID]+''))
-@userProjects = result
+		groupString = ''
+
+		@groupIDs['groups'].each do |group|
+			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Group/' + group['groupID'].to_s)
+			req = Net::HTTP::Get.new(url.path)
+			res = Net::HTTP.start(url.host, url.port) {|http|
+				http.request(req)
+			}
+			groupString = res.body
+
+			@groups.push(JSON.parse(groupString))
+
+		end
+
+
+		# TODO fix backend then uncomment code
+		# urlProjects = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Project/' + cookies[:userID])
+		# req = Net::HTTP::Get.new(urlProjects.path)
+		# res = Net::HTTP.start(urlProjects.host, urlProjects.port) {|http|
+		# 	http.request(req)
+		# }
+		# @projectIDs = JSON.parse(res.body)
+		# @project = Array.new
+
+		# @projectString = ''
+
+		# @projectIDs['project'].each do |project|
+		# 	url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Project/' + group['groupID'].to_s)
+		# 	req = Net::HTTP::Get.new(url.path)
+		# 	res = Net::HTTP.start(url.host, url.port) {|http|
+		# 		http.request(req)
+		# 	}
+		# 	projectString = res.body
+
+		# 	@project.push(JSON.parse(projectString))
+
+		# end
 
 	end
 	def editmyprofile 
