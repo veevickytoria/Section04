00f2ae43cc6cc48f9108a7d5b3bd4e74d3cc468b
smithgb <g_rant@grants-mbp.wlan.rose-hulman.edu>
2014-02-07 08:58:36 -0500

    groups update

--- Blizferno/web_app/app/controllers/groups_controller.rb
+++ Blizferno/web_app/app/controllers/groups_controller.rb
@@ -23,21 +23,22 @@ class GroupsController < ApplicationController
 			http.request(req)
 		}
 
-		groupIDs = JSON.parse(res.body)
-
+		@groupIDs = JSON.parse(res.body)
 		@groups = Array.new
+
 		groupString = ''
 
-		groupIDs['groups'].each do |group|
-			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Group/' + group['id'].to_s)
+		@groupIDs['groups'].each do |group|
+			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Group/' + group['groupID'].to_s)
 			req = Net::HTTP::Get.new(url.path)
 			res = Net::HTTP.start(url.host, url.port) {|http|
 				http.request(req)
 			}
 			groupString = res.body
-			groupString = groupString[0..-2] + ',"groupID":"' + group['id'].to_s + '"}';
+			# groupString = groupString[0..-2] + ',"groupID":"' + group['groupID'].to_s + '"}';
 
 			@groups.push(JSON.parse(groupString))
+
 		end
 	end
 
