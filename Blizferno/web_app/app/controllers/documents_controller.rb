cf1d8b1ce5d270378caf053cff145fd67bd130b5
Jonathan Earles <earlesja@rose-hulman.edu>
2014-02-17 01:05:11 -0500

    it was working - but now it isn't. I hope the world burns.

--- Blizferno/web_app/app/controllers/documents_controller.rb
+++ Blizferno/web_app/app/controllers/documents_controller.rb
@@ -1,12 +1,48 @@
 class DocumentsController < ApplicationController
+
+before_filter :getDocuments
+
 	def index
-		@notes = JSON.parse('{"notes": [{"noteID":"1","title":"title1","description":"@description","content":"content1","dateCreated":"@dateCreated"},
-			{"noteID":"2","title":"title2","description":"@description","content":"content2","dateCreated":"@dateCreated"},
-			{"noteID":"3","title":"title3","description":"@description","content":"content3","dateCreated":"@dateCreated"}]}')
 
-		@agendas = JSON.parse('{"agendas":[{"agendaID":"1","title": "agenda1","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]},
-			{"agendaID":"2","title": "agenda2","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]}]}')
+		 @agendas = JSON.parse('{"agendas":[{"agendaID":"1","title": "agenda1","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]},
+		 	{"agendaID":"2","title": "agenda2","content": [{"topic": "@agendaTopic","time": "@duration","description": "@description","subtopic": [{"topic": "agendaTopic","time": "@duration","description": "@description"}]}]}]}')
 
+
+		if (cookies[:userID].blank?)
+			redirect_to '/login/index'
+			return
+		end
+	end
+
+	def getDocuments
+
+		require 'net/http'
+		@userID = cookies[:userID]
+
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Notes/' + @userID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+
+		@UserNoteIDs = JSON.parse(res.body)
+		@notes = Array.new
+		@notesParsed = Array.new
+
+		noteString = ''
+
+		@UserNoteIDs['notes'].each do |note|
+			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Note/' + note['noteID'].to_s)
+			req = Net::HTTP::Get.new(url.path)
+			res = Net::HTTP.start(url.host, url.port) {|http|
+				http.request(req)
+			}
+			noteString = res.body
+
+			@notes.push(noteString)
+			@notesParsed.push(JSON.parse(noteString))
+
+		end
 	end
 	layout "slate"
 end
--- Blizferno/web_app/app/controllers/preferences_controller.rb
+++ Blizferno/web_app/app/controllers/preferences_controller.rb
@@ -1,9 +1,27 @@
 class PreferencesController < ApplicationController
+
+#before_filter :getUserSettings
+
   def home
   	if (cookies[:userID].blank?)
 		redirect_to '/login/index'
 		return
 	end
   end
+
+  def getUserSettings
+
+   #  @UserID = cookies[:userID]
+
+  	# required 'net/http'
+  	# url = URI.parse('http://csse371-04.csse.rose-hulman.edu/UserSettings/' + @userID)
+  	# req = Net::HTTP::Get.new(url.path)
+  	# res = Net::HTTP.start(url.host, url.port) 
+  	# 		{|http|http.request(req)}
+
+   #  @userSettings = JSON.parse(res.body)
+
+  end
+
   layout 'slate'
 end
