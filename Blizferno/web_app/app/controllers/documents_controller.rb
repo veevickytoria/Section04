1b0dbc9510ac5ba8263c6dd5a401a9638b4e199c
harbisjs <harbisjs@rose-hulman.edu>
2014-02-23 17:06:47 -0500

    basic agenda

--- Blizferno/web_app/app/controllers/documents_controller.rb
+++ Blizferno/web_app/app/controllers/documents_controller.rb
@@ -2,13 +2,8 @@ class DocumentsController < ApplicationController
 
 before_filter :index
 before_filter :getDocuments
-before_filter :getAgendas
 
 	def index
-
-
-
-
 		if (cookies[:userID].blank?)
 			redirect_to '/login/index'
 			return
@@ -18,40 +13,9 @@ before_filter :getAgendas
 	def getDocuments
 
 		require 'net/http'
-		@userID = cookies[:userID]
+		userID = cookies[:userID]
 
-		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Notes/' + @userID)
-		req = Net::HTTP::Get.new(url.path)
-		res = Net::HTTP.start(url.host, url.port) {|http|
-			http.request(req)
-		}
-
-		@UserNoteIDs = JSON.parse(res.body)
-		@notes = Array.new
-		@notesParsed = Array.new
-
-		noteString = ''
-
-		@UserNoteIDs['notes'].each do |note|
-			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Note/' + note['noteID'].to_s)
-			req = Net::HTTP::Get.new(url.path)
-			res = Net::HTTP.start(url.host, url.port) {|http|
-				http.request(req)
-			}
-			noteString = res.body
-
-			@notes.push(noteString)
-			@notesParsed.push(JSON.parse(noteString))
-
-		end
-	end
-
-	def getAgendas
-
-		require 'net/http'
-		@userID = cookies[:userID]
-
-		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Agenda/' + @userID)
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Agenda/' + userID)
 		req = Net::HTTP::Get.new(url.path)
 		res = Net::HTTP.start(url.host, url.port) {|http|
 			http.request(req)
@@ -59,7 +23,38 @@ before_filter :getAgendas
 
 		@agendas = res.body
 		@agendasParsed = JSON.parse(res.body)
-		
+
+		userAgendaIDs = Array.new
+		@agendasParsed.each do |agenda|
+			userAgendaIDs.push(agenda['agendaID'])
+		end
+
+
+
+		url = URI.parse('http://csse371-04.csse.rose-hulman.edu/User/Notes/' + userID)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)
+		}
+
+		userNoteIDs = JSON.parse(res.body)
+		@notes = Array.new
+		@notesParsed = Array.new
+
+		noteString = ''
+
+		userNoteIDs['notes'].each do |note|
+			url = URI.parse('http://csse371-04.csse.rose-hulman.edu/Note/' + note['noteID'].to_s)
+			req = Net::HTTP::Get.new(url.path)
+			res = Net::HTTP.start(url.host, url.port) {|http|
+				http.request(req)
+			}
+			noteString = res.body
+			if !userAgendaIDs.include? note['noteID']
+				@notes.push(noteString)
+				@notesParsed.push(JSON.parse(noteString))
+			end
+		end
 	end
 
 	layout "slate"
