7951096b8ae7b5e54a16be380e852711ea5408ea
watterlm <watterlm@rose-hulman.edu>
2014-03-23 12:11:12 -0400

    Profile, Login, and Registration validation

--- Blizferno/web_app/app/assets/javascripts/login.js
+++ Blizferno/web_app/app/assets/javascripts/login.js
@@ -31,8 +31,11 @@ $(window).load(function() {
 			dataType: 'json',
 			success:function(data){
 				if(data.userID != null){
+					document.getElementById('invalid').style.display = "none";
 					setCookie('userID', data.userID,'7');
 					window.location.href = "/home_page/index";
+				}else{
+					document.getElementById('invalid').style.display = "inline";
 				}
 			}
 		});
@@ -43,31 +46,38 @@ $(window).load(function() {
 	jQuery('#registerForm').bind('submit', function(event){
 		event.preventDefault();
 
-		var regName = document.getElementById('regName');
-		var regEmail = document.getElementById('regEmail');
-		var pass = document.getElementById('p0');
+		// var regName = document.getElementById('regName');
+		// var regEmail = document.getElementById('regEmail');
+		// var pass = document.getElementById('p0');
 
-		if(regName.value == "" || regEmail.value == "" || pass.value == ""){
-			alert("You did not enter the required fields.");
-			window.location.href = "/";
-		}
+		// if(regName.value == "" || regEmail.value == "" || pass.value == ""){
+		// 	alert("You did not enter the required fields.");
+		// 	window.location.href = "/";
+		// }
 
-		var form = this;
-		var json = new ConvertFormToJSON(form);
-		json.password = SHA256(json.password);
+		var invalid = validateRegister();
+		if (!invalid){
+			var emailValid = validateEmail();
+			var phoneValid = validatePhone();
+			if (emailValid && phoneValid){
+				var form = this;
+				var json = new ConvertFormToJSON(form);
+				json.password = SHA256(json.password);
 
-		$.ajax({
-			type: 'POST',
-			url: 'http://csse371-04.csse.rose-hulman.edu/User/',
-			data: JSON.stringify(json),
-			dataType: 'json',
-			success:function(data){
-				if(data.userID != null){
-					setCookie('userID', data.userID,'7');
-					window.location.href = "/home_page/index";
-				}
+				$.ajax({
+					type: 'POST',
+					url: 'http://csse371-04.csse.rose-hulman.edu/User/',
+					data: JSON.stringify(json),
+					dataType: 'json',
+					success:function(data){
+						if(data.userID != null){
+							setCookie('userID', data.userID,'7');
+							window.location.href = "/home_page/index";
+						}
+					}
+				});
 			}
-		});
+		}
 
 		return true;
 	});
@@ -80,4 +90,96 @@ function passCheck(){
 		document.getElementById('p1').style.box-shadow('0 0 5px 4px red');
 	else
 		document.getElementById('p1').style.box-shadow('none');
+}
+
+function validateRegister(){
+	var invalid = false;
+
+	if(document.getElementById("regName").value == ""){
+		document.getElementById("nameR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("nameR").style.display = "none";
+    }
+
+	if(document.getElementById("regEmail").value == ""){
+		document.getElementById("emailR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("emailR").style.display = "none";
+    }
+
+	if(document.getElementById("p0").value == ""){
+		document.getElementById("p0R").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("p0R").style.display = "none";
+    }
+
+	if(document.getElementById("p1").value == ""){
+		document.getElementById("p1R").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("p1R").style.display = "none";
+    }
+
+	if(document.getElementById("phone").value == ""){
+		document.getElementById("phoneR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("phoneR").style.display = "none";
+    }
+
+	if(document.getElementById("company").value == ""){
+		document.getElementById("companyR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("companyR").style.display = "none";
+    }
+
+	if(document.getElementById("title").value == ""){
+		document.getElementById("titleR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("titleR").style.display = "none";
+    }
+
+	if(document.getElementById("location").value == ""){
+		document.getElementById("locationR").style.display = "inline";
+    	invalid = true;
+    }else{
+    	document.getElementById("locationR").style.display = "none";
+    }
+
+
+    return invalid;
+}
+
+function validateEmail(){
+    var x= document.getElementById("regEmail").value;
+    var atpos=x.indexOf("@");
+    var dotpos=x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+    {
+      document.getElementById("emailV").style.display = "inline";
+      return false;
+    }else{
+      document.getElementById("emailV").style.display = "none";
+      return true;
+    }
+}
+
+function validatePhone(){  
+	var x= document.getElementById("phone").value;
+	var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;  
+	if(x.match(phoneno))  
+	{  
+	  document.getElementById("phoneV").style.display = "none";
+	  return true;  
+	}  
+	else  
+	{  
+	  document.getElementById("phoneV").style.display = "inline"; 
+	  return false;  
+	}  
 }
