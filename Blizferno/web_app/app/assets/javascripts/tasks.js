912075c0f0ed6125078e5e2bf8cf38eb833b4809
watterlm <watterlm@rose-hulman.edu>
2014-03-25 20:40:07 -0400

    agenda and note validation

--- Blizferno/web_app/app/assets/javascripts/tasks.js
+++ Blizferno/web_app/app/assets/javascripts/tasks.js
@@ -1,25 +1,45 @@
 function submitCreateTaskModal(){
-		$('#sCrTsk').addClass('disabled');
-    	$('#cCrTsk').addClass('disabled');
-		//grab the form from the html document
-		var form = document.getElementById('createTask');
-		//still testing validation
-		//if(validateMeetingForm(form)){
-		//parse it into a json object
-		var formJSON = new convertFormToJSON(form);
-
+	alert("tasks js")
+	$('#sCrTsk').addClass('disabled');
+	$('#cCrTsk').addClass('disabled');
+	//grab the form from the html document
+	var form = document.getElementById('createTask');
+	//still testing validation
+	//if(validateMeetingForm(form)){
+	//parse it into a json object
+	var formJSON = new convertFormToJSON(form);
+	var invalid = validateNewValues(formJSON);
+	if (!invalid){
 		//manually grab the values for each field expected by the backend
 		var userID = getCookie('userID');
 		var postTitle = formJSON.title;
-		var postisCompleted = formJSON.isCompleted;
+		var postisCompleted = false;
 		var postDescription = formJSON.description;
-		var postDeadline = formJSON.deadline;
-		var postDateCreated = formJSON.dateCreated;
-		var postDateAssigned = formJSON.dateAssigned;
+		
+		var date = formJSON.deadlinedate.split("-");
+		var time = formJSON.deadlinetime.split(":");
+		datetime = new Date(date[0], date[1]-1, date[2], time[0], time[1]);
+		datetime = datetime.getTime()/1000.0;
+		var postDeadline = datetime;
+
+		var dt = formJSON.dateCreated.split(" ");
+		date = dt[0].split("-");
+		time = dt[1].split(":");
+		datetime = new Date(date[0], date[1]-1, date[2], time[0], time[1]);
+		datetime = datetime.getTime()/1000.0;
+		var postDateCreated = datetime;
+
+		dt = formJSON.dateAssigned.split(" ");
+		date = dt[0].split("-");
+		time = dt[1].split(":");
+		datetime = new Date(date[0], date[1]-1, date[2], time[0], time[1]);
+		datetime = datetime.getTime()/1000.0;
+		var postDateAssigned = datetime;
+
 		var postCompletionCriteria = formJSON.completionCriteria;
 		var postAssignedFrom = getCookie('userID');
 		var postCreatedBy = getCookie('userID');
-
+		var postAssignedTo = formJSON.assignedTo;
 
 		var attendees = getMembers('assignedTo');
 		var postAttendance = [];
@@ -37,7 +57,7 @@ function submitCreateTaskModal(){
 			"dateCreated":postDateCreated,
 			"dateAssigned":postDateAssigned,
 			"completionCriteria":postCompletionCriteria,
-			"assignedTo":userID,
+			"assignedTo":postAssignedTo,
 			"assignedFrom":postAssignedFrom,
 			"createdBy":postCreatedBy
 		});
@@ -48,9 +68,32 @@ function submitCreateTaskModal(){
 			url: 'http://csse371-04.csse.rose-hulman.edu/Task/',
 			data: postData,
 			success:function(data){
-				//if successful then close the modal and reload the page
+				// if successful then close the modal and reload the page
 				$('#newTaskModal').modal('hide');
+				alert("Task successfully created! Reloading page...")
 				window.location.reload(true);
 			}
 		});
+	}
+}
+
+
+function validateNewValues(JSONForm){
+	alert("validating")
+	var invalidFields = false;
+
+	for (key in JSONForm){
+		alert(key)
+		if(document.getElementById(key+"R") != null){
+			if(JSONForm[key] == ""){
+				invalidFields = true;
+				document.getElementById(key+"R").style.display = "inline";
+			}
+			else{
+				document.getElementById(key+"R").style.display = "none";
+			}
+		}
+	}
+
+	return invalidFields
 }
